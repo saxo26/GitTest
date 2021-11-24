f4e0b1096bdbbae8d4b03600aa7ea52ef19cc1d9
曾子軒 <shawn@lawbank.com.tw>
2021-11-24 09:05:55 +0800

    Test4

--- GitTest/Controllers/HomeController.cs
+++ GitTest/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GitTest.Controllers
     {
         public ActionResult Index()
         {
-            string ver_05;
+            string ver_04;
 
             return View();
         }
